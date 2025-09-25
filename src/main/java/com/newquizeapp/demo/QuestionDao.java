06ebde7cbc1d81aa9b8762130bcea940ee0c9600
URasanjali <uresharasanjali@gmail.com>
2025-09-25 12:52:38 +0530

    Addquestion feature added

--- src/main/java/com/newquizeapp/demo/QuestionDao.java
+++ src/main/java/com/newquizeapp/demo/QuestionDao.java
@@ -2,8 +2,11 @@ package com.newquizeapp.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionDao extends JpaRepository<Question,Integer> {
 
 
+    List<Question> findByCategory(String Category);
 }
